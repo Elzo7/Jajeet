61da3269453dfcebb523ddf463bc77a342f0c7f4
Maciej Nawrot <maciejnawrot.it@gmail.com>
2023-11-12 13:59:20 +0100

    Initial Commit

---
+++ MemoryGame/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MemoryGame
+//
+//  Created by user249544 on 11/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by user249544 on 11/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
